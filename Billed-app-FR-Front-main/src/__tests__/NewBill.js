403d2841e58f391c0a61630b2f7167fbb858785a
thomas-708 <thomaslap@outlook.fr>
2023-04-05 20:26:49 +0200

    FIX test incomplet

--- Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -2,20 +2,58 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { screen, fireEvent, waitFor } from "@testing-library/dom";
 import userEvent from "@testing-library/user-event";
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import mockStore from "../__mocks__/store.js";
+import router from "../app/Router.js";
+import { ROUTES_PATH } from "../constants/routes";
+import { localStorageMock } from '../__mocks__/localStorage'
+import BillsUI from '../views/BillsUI.js'
+import NewBillUI from '../views/NewBillUI'
+import NewBill from '../containers/NewBill'
+jest.mock("../app/store", () => mockStore);
+
+beforeEach(() => {
+  localStorage.setItem(
+      "user",
+      JSON.stringify({ type: "Employee", email: "a@a" })
+  );
+  const root = document.createElement("div");
+  root.setAttribute("id", "root");
+  document.body.append(root);
+  router();
+  window.onNavigate(ROUTES_PATH.NewBill);
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+  document.body.innerHTML = "";
+});
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("if upload justify file png format", () => {
-      const file = new File(["file"], "file.png", { type: "image/png" });
-    });
+    /**
+     * Test si le file est un format valide
+     */
+    test("I can upload a justified PNG file", async () => {
 
-    test("if upload justify file text format", () => {
+      const file = new File(["file"], "file.png", { type: "image/png" });
+
+      const fileInput = await waitFor(() => screen.getByTestId("file"));
+      userEvent.upload(fileInput, file);
+      expect(fileInput.files[0]).toStrictEqual(file);
+      expect(fileInput.files).toHaveLength(1);
+    });
+    /**
+     * Test si le ficher a un format invalide
+     */
+    test("I cannot upload a justified text file", async () => {
+
       const file = new File(["file"], "file.txt", { type: "text/plain" });
+      const fileInput = await waitFor(() => screen.getByTestId("file"));
+      userEvent.upload(fileInput, file);
+      expect(fileInput.files).toHaveLength(1);
     });
   })
 })
