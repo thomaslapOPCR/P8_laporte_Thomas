fe3e1415b06836215b7ace3ed97c45f71ad215d9
thomas-708 <thomaslap@outlook.fr>
2023-04-05 15:46:10 +0200

    ADD test Images formats REFRACT accept format in html REMOVE Js code accept format in js

--- Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -3,16 +3,19 @@
  */
 
 import { screen } from "@testing-library/dom"
+import userEvent from "@testing-library/user-event";
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
+    test("if upload justify file png format", () => {
+      const file = new File(["file"], "file.png", { type: "image/png" });
+    });
+
+    test("if upload justify file text format", () => {
+      const file = new File(["file"], "file.txt", { type: "text/plain" });
+    });
   })
 })
